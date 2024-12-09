ce1a8e9776a8353041b3aa3e81cc40909377c300
asrahim88 <tasmimhasan1997@gmail.com>
2024-12-09 11:23:18 +0600

    update requarments file

--- SecureBanking/settings.py
+++ SecureBanking/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')  # Keep fallback for development
+
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
